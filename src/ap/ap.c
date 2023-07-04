aff5070b3733b5d30694d08f7f37e21ce935a01e
WANG\WANG <wang8763@naver.com>
2023-07-04 22:46:38 +0900

    램함수 테스트~

--- src/ap/ap.c
+++ src/ap/ap.c
@@ -14,6 +14,60 @@
 #include "pid_init.h"
 #include "sensors.h"
 
+  
+extern uint32_t _siram_code;
+extern uint32_t _sram_code;
+extern uint32_t _eram_code;
+
+void ramfuncInit(void)
+{
+	uint32_t *p_siram_code = &_siram_code;
+	uint32_t *p_sram_code  = &_sram_code;
+	uint32_t *p_eram_code  = &_eram_code;
+	uint32_t lenght;
+
+	lenght = (uint32_t)(p_eram_code - p_sram_code);
+
+	for (int i=0; i<lenght; i++)
+	{
+		p_sram_code[i] = p_siram_code[i];
+	}
+}
+
+
+__attribute__((section(".ram_code"))) void funcRam(void)
+{
+	volatile float sum, a, b;
+
+	sum = 0.;
+	a   = 0.;
+	b   = 0.;
+
+	for(int i=0; i<1024*1024; i++)
+	{
+		sum += a+b;
+		a   += 1.;
+		b   += 2.;
+	}
+}
+
+void funcFlash(void)
+{
+	volatile float sum, a, b;
+
+	sum = 0.;
+	a   = 0.;
+	b   = 0.;
+
+	for(int i=0; i<1024*1024; i++)
+	{
+		sum += a+b;
+		a   += 1.;
+		b   += 2.;
+	}
+}
+
+
 void apInit(void)
 {
 	cliOpen(_DEF_USB, 57600);
@@ -29,13 +83,35 @@ void apInit(void)
 	// Finally initialize the gyro filtering
     gyroInitFilters();
 	pidInit();
+	ramfuncInit();
 }
 
 void apMain(void)
 {
+	uint32_t pre_time = millis();
 	while(1)
 	{
-		scheduler();
+		//scheduler();
+
+		if (millis()-pre_time >= 1500)
+		{
+			pre_time = millis();
+			ledToggle(_DEF_LED1);
+
+			uint32_t pre_time_func;
+			uint32_t exe_time;
+
+			pre_time_func = millis();
+			funcRam();
+			exe_time = millis() - pre_time_func;
+			cliPrintf("funRam     : %d ms\n", exe_time);
+
+			pre_time_func = millis();
+			funcFlash();
+			exe_time = millis() - pre_time_func;
+			cliPrintf("funFlash   : %d ms\n", exe_time);
+		}
+
 	}
 	// 	if (millis()-pre_time >= 1000)
     // 	{
